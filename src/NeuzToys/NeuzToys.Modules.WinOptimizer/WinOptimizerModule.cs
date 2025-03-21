3cc8ddcf14b04e1b12ac69b0d64fbc71e8e5502c
viacooky <viacooky@qq.com>
2025-03-22 02:11:42 +0800

    feat: 完成系统信息功能

---
+++ src/NeuzToys/NeuzToys.Modules.WinOptimizer/WinOptimizerModule.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using NeuzToys.Modules.WinOptimizer.Services;
+using NeuzToys.Modules.WinOptimizer.ViewModels;
+using NeuzToys.Modules.WinOptimizer.Views;
+using NeuzToys.Shared;
+using NeuzToys.Shared.Models;
+using NeuzToys.Shared.Services;
+
+namespace NeuzToys.Modules.WinOptimizer;
+
+public class WinOptimizerModule : IModule
+{
+    public IServiceCollection ConfigureServices(IServiceCollection services)
+    {
+        return services
+            .AddSingleton<WmiService>()
+            .AddSingleton<SystemInfoViewModel>();
+    }
+
+    public void ConfigureMenuService(MenuService menuService)
+    {
+        menuService.AddMenu(new MenuItem
+        {
+            Key = "WinOptimizer",
+            Name = "系统优化",
+            Icon = NeuzIcons.CommonKeys.Windows,
+            Children =
+            [
+                new MenuItem
+                {
+                    Key = "WinOptimizer_SystemInformation",
+                    Name = "系统信息",
+                    Icon = NeuzIcons.CommonKeys.Information,
+                    ViewType = typeof(SystemInfoView),
+                    ViewModelType = typeof(SystemInfoViewModel)
+                },
+                new MenuItem
+                {
+                    Key = "WinOptimizer_Performance",
+                    Name = "性能",
+                    Icon = NeuzIcons.CommonKeys.Performance
+                }
+            ]
+        });
+    }
+}
